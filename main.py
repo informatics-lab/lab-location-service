d4baaf9e26a34eba35300f042113353c0d50fdef
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-08-01 12:41:44 +0100

    Add mongo support

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from datetime import datetime
 import os
 import sys
+import time
 
 from flask import Flask, request
 from flask import jsonify
+import pymongo
+from pymongo import MongoClient
+
 app = Flask(__name__)
+db_connected = False
+
+print("Connecting to database.")
+
+while db_connected is not True:
+    try:
+        client = MongoClient(os.environ.get('MONGO_HOST', 'localhost'),
+                             os.environ.get('MONGO_PORT', 27017))
+        client.server_info()
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print("Unable to connect to mongo, trying again in 5 seconds")
+        time.sleep(5)
+        continue
+    db_connected = True
+
+db = client[os.environ.get('MONGO_DB', 'lab-locations')]
+locations = db['locations']
 
 user_locations = {}
 
@@ -14,26 +35,50 @@ except KeyError:
     print("Need to set env var AUTH_TOKEN")
     sys.exit(1)
 
+def check_connection():
+    return db_connected
+
+def get_location(username):
+    location = locations.find_one({"username": username})
+    if location is None:
+        raise KeyError("Cannot find user %s", username)
+    if location['update_time'].date() != datetime.today().date():
+        location["location"] = "unknown"
+    return location
+
+def set_location(username, location):
+    try:
+        document = get_location(username)
+    except KeyError:
+        document = {}
+    document['username'] = username
+    document['location'] = location
+    document['update_time'] = datetime.now()
+    locations.insert_one(document)
+
 @app.route('/')
 def hello_world():
+    if check_connection() is False:
+        return jsonify(message='Database is not connected'), 500
     return jsonify(message='Welcome to the Lab location service.')
 
 @app.route('/user/<username>', methods=['GET'])
 def show_user_location(username):
+    if check_connection() is False:
+        return jsonify(message='Database is not connected'), 500
     try:
-        location = user_locations[username]['location']
-        if user_locations[username]['update_time'].date() != datetime.today().date():
-            raise ValueError('That location is out of date')
+        location = get_location(username)['location']
     except (KeyError, ValueError):
-        location = "Unknown"
+        location = "unknown"
     return jsonify(username=username, location=location)
 
 @app.route('/user/<username>', methods=['POST'])
 def set_user_location(username):
+    if check_connection() is False:
+        return jsonify(message='Database is not connected'), 500
     if request.headers.get('Authorization') == "Bearer {}".format(AUTH_TOKEN):
         info = request.get_json()
-        user_locations[username] = {'location': info["location"],
-                                    'update_time': datetime.now()}
+        set_location(username, info["location"])
         print(info["location"])
         return "ok"
     else:
